090b08e087a72b4b6ba6574dada8beea0938becd
anupjha184501-web <anupjha184501@gmail.com>
2026-08-05 20:44:05 +0530

    4

---
+++ BACKEND/src/index.js
@@ -0,0 +1,141 @@
+  import mongoose from "mongoose";
+  import {DB_NAME} from "/.constants.js";
+ import experess from "express";
+  const app = experess();
+
+//   aprroach no 1
+//   ( async () => {
+//     try{
+//       await mongoose.connect($`{process.env.MONGO_URI}/${DB_NAME}`, );
+//       console.log("Connected to MongoDB");
+//       app.on("error", (err) => {
+//         console.error("Express app error:", err);
+//       });
+
+//       app.listen(process.env.PORT || 3000, () => {
+//         console.log("Express app is listening on port $`{process.env.PORT || 3000}`");
+//       })
+//     } catch (error) {
+//       console.error("Error connecting to MongoDB:", error);
+//     }
+//   })()
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
